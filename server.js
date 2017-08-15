fe42a200998d040ae5754650581cc34b78d64047
Sean <sean@Sens-MacBook-Pro.local>
2017-08-14 20:48:18 -0500

    hbs partial & helpers

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase()
+});
 
 app.get('/', (req, res) => {
   // res.send('<h1>Hello Express!</h1>');
@@ -17,16 +26,15 @@ app.get('/', (req, res) => {
   // })
   res.render('home.hbs', {
     pageTitle: 'Home Page',
-    currentYear: new Date().getFullYear(),
     welcomeMessage: 'Welcome to my site!'
   })
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
-  })
+    pageTitle: 'About Page'
+    // currentYear: new Date().getFullYear()
+  })  //^ moved to registerHelper
 });
 
 app.get('/bad', (req, res) => {
